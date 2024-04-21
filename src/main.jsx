c4a54910b3aa1bc9bbcfbf906202ff10aa055468
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-04-21 19:45:29 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import "modern-normalize";
 import { Provider } from 'react-redux';
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from 'redux-persist/integration/react';
